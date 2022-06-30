2631d06da41e7d2a4934247066eab7925dff6ef4
ATH42 <alex.t.hoffmann@icloud.com>
2022-06-30 21:36:22 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// basic logic to to show how to run node cli
+
+const amount = 9;
+
+console.log(amount);
+
+if (amount < 10) {
+  console.log(`${amount} is less than 10`);
+}
+
+if (amount > 10) {
+  console.log(amount + "is more than 10");
+}
+
+console.log(`end of program`);
