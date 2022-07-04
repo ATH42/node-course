74760169e9c189fbbebe691f38d90b7fe7a4b2d5
ATH42 <alex.t.hoffmann@icloud.com>
2022-07-04 22:27:35 +0200

    feat(app\route): set up new route with response;

--- 01_Task_Manager/starter/app.js
+++ 01_Task_Manager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+// app.get('/api/v1/tasks')
+// app.post('/api/v1/tasks')
+// app.get('/api/v1/tasks/:id')
+// app.patch('/api/v1/tasks/:id')
+// app.delete('/api/v1/tasks/:id')
+
+const port = 3000;
+
+app.listen(port, console.log(`server is listening on port ${port}...`));
